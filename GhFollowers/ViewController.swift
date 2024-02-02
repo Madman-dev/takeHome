df68f93fb8c02f361ea35ecef1268774b1d43477
7과11사이 <dongjunlee222@gmail.com>
2024-02-02 15:34:12 +0900

    [Feat]: Initial Setup

---
+++ GhFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GhFollowers
+//
+//  Created by Porori on 2/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
