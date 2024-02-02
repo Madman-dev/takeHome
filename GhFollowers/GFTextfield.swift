02a81c0cc51a98e907b1b82f90344a88a81701f2
7과11사이 <dongjunlee222@gmail.com>
2024-02-02 17:45:16 +0900

    [Feat]: created Custom Button

---
+++ GhFollowers/GFTextfield.swift
@@ -0,0 +1,39 @@
+//
+//  GFTextfield.swift
+//  GhFollowers
+//
+//  Created by Porori on 2/2/24.
+//
+
+import UIKit
+
+class GFTextfield: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true // automatic resize
+        minimumFontSize = 12 // limits the font minimum font size
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+}
