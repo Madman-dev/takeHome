4a4ed92ee3db942417a4d9982afaa7e4fa4ba3f0
7과11사이 <dongjunlee222@gmail.com>
2024-02-04 15:13:50 +0900

    [ADD]: TitleLabel Component

---
+++ GhFollowers/GFTitleLabel.swift
@@ -0,0 +1,37 @@
+//
+//  GFTitleLabel.swift
+//  GhFollowers
+//
+//  Created by Porori on 2/4/24.
+//
+
+import UIKit
+
+// UIComponent를 이렇게 구분하는구나
+class GFTitleLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        // Always Bold
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
