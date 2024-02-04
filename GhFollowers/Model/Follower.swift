83ba3dcdde704b21cf50a852c70b5763fe6f1b7f
7과11사이 <dongjunlee222@gmail.com>
2024-02-04 18:28:25 +0900

    [ADD]: user,follower Model

---
+++ GhFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GhFollowers
+//
+//  Created by Porori on 2/4/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatar_url: String
+}
