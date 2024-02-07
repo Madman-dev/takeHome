559d4d8436744de075e0835a851fd6d69859fe4a
7과11사이 <dongjunlee222@gmail.com>
2024-02-07 18:41:58 +0900

    [ADD]: Cell

---
+++ GhFollowers/CustomViews/Cells/FollowerCell.swift
@@ -0,0 +1,46 @@
+//
+//  FollowerCell.swift
+//  GhFollowers
+//
+//  Created by Porori on 2/7/24.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseId = "FollowerCell"
+    
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(followers: Follower) {
+        usernameLabel.text = followers.login
+    }
+    
+    private func configure() {
+        [avatarImageView, usernameLabel].forEach { addSubview($0) }
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
