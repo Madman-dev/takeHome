b767ef8b7024375305093ee7ec97ad387d9107e9
7과11사이 <dongjunlee222@gmail.com>
2024-02-07 19:42:52 +0900

    [ADD]: Collectionview Flowlayout

--- GhFollowers/Screens/FollowerListVC.swift
+++ GhFollowers/Screens/FollowerListVC.swift
@@ -34,7 +34,7 @@ class FollowerListVC: UIViewController {
     }
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowlayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseId)
@@ -50,5 +50,19 @@ class FollowerListVC: UIViewController {
                 self.presentGFAlertOnMainThread(title: "오류", message: error.rawValue, buttonTitle: "OK")
             }
         }
+    }
+    
+    // THE VC doesn't HAVE to know about the flowlayout. > Does it have to know?
+    func createThreeColumnFlowlayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        return flowLayout
     }
 }
