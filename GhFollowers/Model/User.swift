d975c531aedc99794fb4e941eff2f68ff321abaa
7과11사이 <dongjunlee222@gmail.com>
2024-02-16 11:52:52 +0900

    [ADD]: UserInfo Network call

--- GhFollowers/Model/User.swift
+++ GhFollowers/Model/User.swift
@@ -7,16 +7,17 @@
 
 import Foundation
 
+// out app is only able to pull down the data on the web
 struct User: Codable {
-    var login: String
-    var avatarUrl: String
+    let login: String
+    let avatarUrl: String
     var name: String?
     var location: String?
     var bio: String?
-    var publicRepos: Int
-    var publicGists: Int
-    var htmlUrl: String
-    var following: Int
-    var followers: Int
-    var createdAt: String
+    let publicRepos: Int
+    let publicGists: Int
+    let htmlUrl: String
+    let following: Int
+    let followers: Int
+    let createdAt: String
 }
--- GhFollowers/Screens/UserInfoVC.swift
+++ GhFollowers/Screens/UserInfoVC.swift
@@ -17,7 +17,17 @@ class UserInfoVC: UIViewController {
         // different types to initiate UIBarButtonItem && UIComponent
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
-        print(username!)
+        
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
     @objc func dismissVC() {
