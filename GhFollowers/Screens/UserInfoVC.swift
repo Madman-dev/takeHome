6c781efe0d6bd01a611f9fce5a5d70a483a6d182
7과11사이 <dongjunlee222@gmail.com>
2024-02-19 14:23:03 +0900

    [ADD]: DateLabel below UserInfoVC

--- GhFollowers/Screens/UserInfoVC.swift
+++ GhFollowers/Screens/UserInfoVC.swift
@@ -12,6 +12,7 @@ class UserInfoVC: UIViewController {
     let headerView = UIView()
     let itemViewOne = UIView()
     let itemViewTwo = UIView()
+    let dateLabel = GFBodyLabel(textAlignment: .center)
     var itemViews: [UIView] = []
     
     var username: String!
@@ -41,6 +42,9 @@ class UserInfoVC: UIViewController {
                     self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
                     self.add(childVC: GFRepoItemVC(user: user), to: self.itemViewOne)
                     self.add(childVC: GFFollowerItemVC(user: user), to: self.itemViewTwo)
+                    
+                    // self.dateLabel.text = user.createdAt // 사용자 github profile이 생성된 일정
+                    self.dateLabel.text = "Using Github since, \(user.createdAt.convertToDisplayFormat())"
                 }
             case .failure(let error):
                 print(error.localizedDescription)
@@ -52,7 +56,9 @@ class UserInfoVC: UIViewController {
         let padding: CGFloat = 20
         let itemHeight: CGFloat = 140
         
-        itemViews = [headerView, itemViewOne, itemViewTwo]
+        // 날짜 레이블 추가
+        itemViews = [headerView, itemViewOne, itemViewTwo, dateLabel]
+        
         for itemView in itemViews {
             view.addSubview(itemView)
             itemView.translatesAutoresizingMaskIntoConstraints = false
@@ -72,6 +78,9 @@ class UserInfoVC: UIViewController {
             
             itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
             itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight),
+            
+            dateLabel.topAnchor.constraint(equalTo: itemViewTwo.bottomAnchor, constant: padding),
+            dateLabel.heightAnchor.constraint(equalToConstant: 18)
         ])
     }
     
