989154a42a5400cfcc5240575207b387811757be
7과11사이 <dongjunlee222@gmail.com>
2024-02-20 17:38:00 +0900

    [REFACTOR]: AlertVC containerView

---
+++ GhFollowers/CustomViews/Views/GFAlertContainerView.swift
@@ -0,0 +1,29 @@
+//
+//  GFAlertContainerView.swift
+//  GhFollowers
+//
+//  Created by Porori on 2/20/24.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // GFAlertVC didn't have to know about the following containerView code. therefore refactored
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
