3f025058c38d96a9b13eae90032eb40a8b1d724d
7과11사이 <dongjunlee222@gmail.com>
2024-02-21 16:42:56 +0900

    [FIX]: potential tableview data sync error

--- GhFollowers/Screens/FavoriteVC.swift
+++ GhFollowers/Screens/FavoriteVC.swift
@@ -87,14 +87,14 @@ extension FavoriteVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         guard editingStyle == .delete else { return }
         
-        let favorite = favorites[indexPath.row]
-        favorites.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .fade)
-        
         // update the Userdefault data
-        PersistenceManager.updateWith(follower: favorite, actionType: .remove) { [weak self] error in
+        PersistenceManager.updateWith(follower: favorites[indexPath.row], actionType: .remove) { [weak self] error in
             guard let self = self else { return }
-            guard let error = error else { return } // no error
+            guard let error = error else {
+                self.favorites.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+                return
+            } // when error occurs, the current code structure would make data out of sync
             self.presentGFAlertOnMainThread(title: "오류가 발생했습니다.", message: error.rawValue, buttonTitle: "Ok")
         }
     }
